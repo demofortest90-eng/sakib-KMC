3150e27389b84daf5619d44a807df57bc4917fef
demofortest90-eng <demofortest90@gmail.com>
2026-06-09 08:44:14 +0600

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,4 @@ from app import create_app, socketio
 app = create_app()
 
 if __name__ == "__main__":
-    # debug=True for development. Use a production server for deployment.
     socketio.run(app, host="0.0.0.0", port=5000, debug=True, allow_unsafe_werkzeug=True)
